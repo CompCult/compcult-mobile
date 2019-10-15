02ba1c032f0d9602497d0070f38155f20fd46b87
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2019-10-15 10:14:32 -0300

    Tela de lista de missoes

--- lib/pages/missions_page.dart
+++ lib/pages/missions_page.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+import '../shared/components/item_card.dart';
+import './mission_submit.dart';
+import '../shared/models/mission.dart';
 
 class MissionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Missões'));
+    return FutureBuilder(
+      future: _getMissions(),
+      builder: (_, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: SizedBox(
+              width: 50,
+              height: 50,
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        final Response response = snapshot.data;
+
+        final List<Mission> missions = List<Mission>.from(
+            response.data.map((mission) => Mission.fromJson(mission)));
+
+        return _buildList(missions);
+      },
+    );
+  }
+
+  _buildList(List<Mission> missions) {
+    return ListView.builder(
+      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 15),
+      itemCount: missions.length,
+      itemBuilder: (_, i) {
+        final mission = missions[i];
+
+        return ItemCard(
+          mission: mission,
+          routeName: MissionSubmit.routeName,
+        );
+      },
+    );
+  }
+
+  Future<Response> _getMissions() {
+    return Dio()
+        .get('https://museu-vivo-api.herokuapp.com/missions/public?user_id=4');
   }
 }
---
+++ lib/shared/components/item_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:museu_vivo/shared/models/mission.dart';
+
+class ItemCard extends StatelessWidget {
+  final Mission mission;
+  final String routeName;
+
+  ItemCard({this.mission, this.routeName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      elevation: 2,
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pushNamed(routeName, arguments: mission);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                mission.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '${mission.description.substring(0, 140)}...',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
