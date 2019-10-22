262ef44739d310a84ced263000cf08915e356e0c
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2019-10-22 08:12:56 -0300

    Pontos do usuario

--- lib/pages/coins_page.dart
+++ lib/pages/coins_page.dart
@@ -1,12 +1,28 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:museu_vivo/shared/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 class CoinsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final int userId = Provider.of<UserProvider>(context).userId;
+
     return Container(
       child: Center(
-        child: Text("CoinsPage"),
-      ),
+          child: FutureBuilder(
+        future: _getCoins(userId),
+        builder: (_, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+          return Text('${snapshot.data.data['points']}');
+        },
+      )),
     );
+  }
+
+  Future<Response> _getCoins(int userId) {
+    return Dio().get('https://museu-vivo-api.herokuapp.com/users/$userId');
   }
 }
