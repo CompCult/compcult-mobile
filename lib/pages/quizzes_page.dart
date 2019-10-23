bc1b4d4ec1825ebdc9d8bdfd677cebd6dd4d8619
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2019-10-23 17:29:40 -0300

    Codigo secreto do quiz

--- lib/pages/quizzes_page.dart
+++ lib/pages/quizzes_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:museu_vivo/pages/quiz_submit.dart';
+import 'package:museu_vivo/shared/components/secret_code_field.dart';
 import 'package:museu_vivo/shared/models/quiz.dart';
 import 'package:provider/provider.dart';
 
@@ -13,35 +14,56 @@ class QuizzesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final int userId = Provider.of<UserProvider>(context).userId;
 
-    return FutureBuilder(
-      future: _getQuizzes(userId),
-      builder: (_, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-            child: SizedBox(
-              width: 50,
-              height: 50,
-              child: CircularProgressIndicator(),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 15),
+        child: Column(
+          children: <Widget>[
+            SecretCodeField(
+              label: 'Código secreto do quiz',
+              onSubmited: (quizId) async {
+                Response response = await Dio().get(
+                    'https://museu-vivo-api.herokuapp.com/quizzes/private?secret_code=$quizId');
+                Quiz quiz = Quiz.fromJson(response.data);
+
+                Navigator.of(context)
+                    .pushNamed(QuizSubmit.routeName, arguments: quiz);
+              },
             ),
-          );
-        }
+            SizedBox(height: 15),
+            FutureBuilder(
+              future: _getQuizzes(userId),
+              builder: (_, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
 
-        final Response response = snapshot.data;
+                final Response response = snapshot.data;
 
-        final List<Quiz> quizzes =
-            List<Quiz>.from(response.data.map((quiz) => Quiz.fromJson(quiz)));
-
-        return _buildList(quizzes);
-      },
+                final List<Quiz> quizzes = List<Quiz>.from(
+                    response.data.map((quiz) => Quiz.fromJson(quiz)));
+                return _buildList(quizzes);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  _buildList(List<Quiz> Quizzes) {
+  _buildList(List<Quiz> quizzes) {
     return ListView.builder(
-      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 15),
-      itemCount: Quizzes.length,
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: quizzes.length,
       itemBuilder: (_, i) {
-        final quiz = Quizzes[i];
+        final quiz = quizzes[i];
 
         return ItemCard(
           item: quiz,
