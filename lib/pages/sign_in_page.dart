39ab585cd8e32557698efe5f27c2d3ff6f074f7b
Lucas Christopher <lcsilva13@stefanini.com>
2020-02-25 14:26:45 -0300

    refactor: refatoracao no layout e na logica das telas de login e cadastro

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,9 +1,10 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:museu_vivo/shared/components/button_submit_form.dart';
+import 'package:museu_vivo/shared/components/text_form_field.dart';
 
 import 'bloc/sign_in_bloc.dart';
 import 'home_page.dart';
-import 'reset_password_page.dart';
 import 'sign_up_page.dart';
 import '../config.dart';
 
@@ -25,175 +26,100 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        color: Colors.white,
-        child: ListView(
-          padding: EdgeInsets.fromLTRB(40, 60, 40, 10),
-          children: <Widget>[
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  // Para redimensionar a logo.
-                  SizedBox(
-                    width: 250,
-                    height: 250,
-                    child: Image.asset(
-                        'assets/${config.assetsDirectoryName}/logo.png'),
-                  ),
-                  SizedBox(height: 5),
-                  if (_showErrorMessage)
-                    Text(
-                      'Usuário ou senha incorretos',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).accentColor,
-                      ),
+      body: GestureDetector(
+        onTap: () {
+          // Esconde o teclado caso esteja aberto ao clicar na tela
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: Container(
+          color: Color(0xFF2d91e7),
+          child: ListView(
+            padding: EdgeInsets.fromLTRB(40, 50, 40, 10),
+            children: <Widget>[
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    // Para redimensionar a logo
+                    SizedBox(
+                      width: 120,
+                      height: 120,
+                      child: Image.asset(
+                          'assets/${config.assetsDirectoryName}/icon.png'),
                     ),
-                  SizedBox(height: 15),
-                  _buildFormField("E-mail", _emailController, false),
-                  SizedBox(height: 10),
-                  _buildFormField("Senha", _passwordController, true),
-                  SizedBox(height: 15),
-                  _buildButton("ENTRAR", context),
-                  SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      GestureDetector(
-                        child: Text(
-                          "Cadastre-se",
-                          style: TextStyle(
-                            fontFamily: "PTSans",
-                            fontWeight: FontWeight.w700,
-                          ),
+                    SizedBox(height: 10),
+                    if (_showErrorMessage)
+                      Text(
+                        'Usuário ou senha incorretos',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).accentColor,
                         ),
-                        // Aqui estarah a funcao para efetuar cadastro...
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SignUpPage(),
-                            ),
-                          );
-                        },
                       ),
-                      GestureDetector(
-                        child: Text(
-                          "Recuperar senha",
-                          style: TextStyle(
-                            fontFamily: "PTSans",
-                            fontWeight: FontWeight.w700,
-                          ),
+                    SizedBox(height: 10),
+                    buildFormField("E-mail", _emailController, false),
+                    SizedBox(height: 10),
+                    buildFormField("Senha", _passwordController, true),
+                    SizedBox(height: 30),
+                    buildButton("ENTRAR", context, _isLoading, _submitLogin),
+                    SizedBox(height: 15),
+                    GestureDetector(
+                      child: Text(
+                        "Cadastre-se",
+                        style: TextStyle(
+                          fontFamily: "PTSans",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
                         ),
-                        // Aqui estarah a funcao para recuperar senha...
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ResetPasswordPage(),
-                            ),
-                          );
-                        },
                       ),
-                    ],
-                  ),
-                ],
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignUpPage(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildFormField(String label, TextEditingController controller,
-      bool permissionToObscure) {
-    return TextFormField(
-      controller: controller,
-      keyboardType: permissionToObscure == true
-          ? TextInputType.text
-          : TextInputType.emailAddress,
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: TextStyle(
-          color: Colors.black38,
-          fontFamily: "Poppins",
-          fontWeight: FontWeight.w500,
-          fontSize: 15,
-        ),
-        border: OutlineInputBorder(),
-        prefixIcon: permissionToObscure == true
-            ? Icon(Icons.lock_outline)
-            : Icon(Icons.mail_outline),
-      ),
-      style: TextStyle(
-        fontSize: 13,
-        fontFamily: "Poppins",
-      ),
-      validator: (value) {
-        if (value.isEmpty)
-          return "O campo \"${label.toLowerCase()}\" não pode ser vazio";
-      },
-      obscureText: permissionToObscure,
-    );
-  }
-
-  Widget _buildButton(String label, BuildContext context) {
+  _submitLogin() async {
     SignInBloc signInBloc = BlocProvider.getBloc<SignInBloc>();
 
-    return Container(
-      height: 50,
-      alignment: Alignment.centerLeft,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          stops: [0.3, 1],
-          colors: [
-            Theme.of(context).accentColor.withOpacity(0.9),
-            Theme.of(context).accentColor,
-          ],
-        ),
-        borderRadius: BorderRadius.all(
-          Radius.circular(5),
-        ),
-      ),
-      child: SizedBox.expand(
-        child: FlatButton(
-          child: _isLoading
-              ? CircularProgressIndicator()
-              : Text(
-                  label,
-                  style: TextStyle(
-                    fontFamily: "Poppins",
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-          onPressed: () async {
-            if (_formKey.currentState.validate()) {
-              try {
-                setState(() {
-                  _isLoading = true;
-                });
-                await signInBloc.authenticate(
-                    _emailController.text, _passwordController.text);
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(HomePage.routeName, (_) => false);
-              } catch (exception) {
-                setState(() {
-                  _showErrorMessage = true;
-                  _isLoading = false;
-                });
-              }
-            }
-          },
-        ),
-      ),
-    );
+    // Esconde o teclado caso esteja aberto ao clicar no botão
+    FocusScopeNode currentFocus = FocusScope.of(context);
+
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+
+    if (_formKey.currentState.validate()) {
+      try {
+        setState(() {
+          _isLoading = true;
+        });
+        await signInBloc.authenticate(
+            _emailController.text, _passwordController.text);
+        Navigator.of(context)
+            .pushNamedAndRemoveUntil(HomePage.routeName, (_) => false);
+      } catch (exception) {
+        setState(() {
+          _showErrorMessage = true;
+          _isLoading = false;
+        });
+      }
+    }
   }
 }
